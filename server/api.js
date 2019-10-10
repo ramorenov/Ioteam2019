cfe307cb00d0b87ba147c6808e979f6ead1a2119
Ricardo Andres Moreno <octopus@MacBook-Pro-de-Ricardo.local>
2019-10-10 16:26:03 -0500

    reset ok

--- server/api.js
+++ server/api.js
@@ -18,7 +18,7 @@ module.exports = function(app) {
 
   app.get("/reset", (req, res, next) => {
     const { body } = req;
-    const newbody = { deviceId: "rasp001" };
+    const newbody = [{ deviceId: "rasp001" }];
     sensorsData = newbody;
     return res.status(201).json(body);
   });
